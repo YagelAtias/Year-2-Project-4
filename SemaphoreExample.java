9c96089b15ab4724fb4f638328649c4e3220b625
Yagel Atias <yagelatias@outlook.com>
2024-02-21 14:07:30 +0200

    Adding main method

--- SemaphoreExample.java
+++ SemaphoreExample.java
@@ -8,12 +8,12 @@ public class SemaphoreExample {
         public void accessResource(){
             try{
                 semaphore.acquire();
-                System.out.println(Thread.currentThread().getName() + "is accessing the resource.");
+                System.out.println(Thread.currentThread().getName() + " is accessing the resource.");
                 Thread.sleep(1000);
             } catch(InterruptedException e){
                 e.printStackTrace();
             } finally {
-                System.out.println(Thread.currentThread().getName() + "is releasing the resource.");
+                System.out.println(Thread.currentThread().getName() + " is releasing the resource.");
                 semaphore.release();
             }
         }
@@ -28,5 +28,14 @@ public class SemaphoreExample {
         public void run(){
             sharedResource.accessResource();
         }
+    }
+    public static void main(String[] args){
+        SharedResource sharedResource = new SharedResource(2);
+        Thread thread1 = new Worker("Thread1",sharedResource);
+        Thread thread2 = new Worker("Thread2",sharedResource);
+        Thread thread3 = new Worker("Thread3",sharedResource);
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
